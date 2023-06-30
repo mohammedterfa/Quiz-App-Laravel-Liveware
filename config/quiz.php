e62dcddbdaf1b873f38273edc6a89a13b459e807
mohammedterfa <mohammedterfa@gmail.com>
2023-06-30 23:52:09 +0200

    make a page where users will finally be able to take quizzes.

---
+++ config/quiz.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'secondsPerQuestion' => 60,
+];
+
--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use App\Http\Livewire\Quiz\QuizList;
 |
 */
 Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('quiz/{quiz}/{slug?}', [HomeController::class, 'show'])->name('quiz.show');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
