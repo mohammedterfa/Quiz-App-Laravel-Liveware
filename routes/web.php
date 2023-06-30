228aa4301c834a9615dce059e4ba5029e127fdbe
mohammedterfa <mohammedterfa@gmail.com>
2023-07-01 01:06:43 +0200

    show user how did they do in the quiz, also adding the leaderboard.

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ResultController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Livewire\Questions\QuestionList;
 use App\Http\Livewire\Questions\QuestionForm;
@@ -20,6 +21,7 @@ use App\Http\Livewire\Quiz\QuizList;
 */
 Route::get('/', [HomeController::class, 'index'])->name('home');
 Route::get('quiz/{quiz}/{slug?}', [HomeController::class, 'show'])->name('quiz.show');
+Route::get('results/{test}', [ResultController::class, 'show'])->name('results.show');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
